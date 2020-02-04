294d642fe0e298ca1adbf0d1a994ba3e42638ef8
Khalid <kabcoder@gmail.com>
2020-02-04 17:20:12 +0100

    budget app with javascript

---
+++ app.js
@@ -0,0 +1,22 @@
+var budgetController = (function(){
+    x = 23;
+    
+    var add = function(b){
+        return b + x;
+    }
+    
+    return {
+        pubilictest: function(){
+            console.log(add(5))
+        }
+    }
+    
+})()
+
+
+var uiController = (function(){
+//    something
+    
+    
+    
+})
